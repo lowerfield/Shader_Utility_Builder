a8b5786e77b3d995e6bc15c5e52d48c1d2b53fc9
lowerfield@gmail.com <manuelperez@manuels-mbp.config>
2015-05-02 12:29:56 +0100

    initial python commit

---
+++ shaderUtilityBuilder.py
@@ -0,0 +1,47 @@
+# node positioning function
+
+def setPos(inputNode, node, x = 0, y = 100):
+
+    nodeXYPos = {'xpos' : int(inputNode['xpos'].value()), 
+                'ypos' : int(inputNode['ypos'].value())}
+
+    node.setXYpos(int(inputNode['xpos'].value()) + x, int(inputNode['ypos'].value()) + y)
+    
+    return
+
+def main():
+
+    # Create shader build group 
+    try:
+        read = nuke.selectedNode()
+        if read.Class() == 'PostageStamp': # will need to change class to Read
+            read = read
+        else:
+            nuke.message('Not posibble to build shader from selected node')
+    
+        group = nuke.nodes.Group(name = 'Shader_Build', postage_stamp = 'True', inputs =[read])
+        setPos(read,group)
+        
+        # begin group
+        group.begin()
+        
+        groupInput = nuke.nodes.Input()
+        groupOutput = nuke.nodes.Output(inputs = [groupInput])
+        
+        group.end()
+
+    except:
+        nuke.message('No node selected')
+
+main()
+
+
+
+
+
+
+
+
+
+
+
